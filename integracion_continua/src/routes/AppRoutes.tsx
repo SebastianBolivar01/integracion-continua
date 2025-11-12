a1be66afaec82cc53a1113074558b80c96886919
Santiago2509 <santiagourubina2509@gmail.com>
2025-11-12 17:32:46 -0500

    añado technologylogic

--- integracion_continua/src/routes/AppRoutes.tsx
+++ integracion_continua/src/routes/AppRoutes.tsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 
-
-
 // ✅ Nuevas vistas educativas 3D
 import ShapesView from "../views/ShapesView";
 import GlobeView from "../views/GlobeView";
-import PaintView from "../views/PaintView";
 import SolarSystemView from "../views/SolarSystemView";
 import WaterCycleView from "../views/WaterCycleView";
+import TechnologyLogicView from "../views/TechnologyLogicView"; // Ruta correcta
+
 
 export default function AppRoutes() {
   return (
@@ -20,9 +19,11 @@ export default function AppRoutes() {
         {/* ✅ Nuevas rutas de las tres áreas temáticas */}
         <Route path="shapes" element={<ShapesView />} />
         <Route path="globe" element={<GlobeView />} />
-        <Route path="paint" element={<PaintView />} />
         <Route path="solarsystem" element={<SolarSystemView />} />
         <Route path="watercycle" element={<WaterCycleView />} />
+
+        {/* Nueva ruta para Tecnología y Pensamiento Lógico */}
+        <Route path="technology-logic" element={<TechnologyLogicView />} />
       </Route>
     </Routes>
   );
---
+++ integracion_continua/src/views/TechnologyLogicView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TechnologyLogic from '../components/TechnologyLogic';
+
+const TechnologyLogicView = () => {
+  return (
+    <div className="h-full p-6">
+      <TechnologyLogic />
+    </div>
+  );
+};
+
+export default TechnologyLogicView;
